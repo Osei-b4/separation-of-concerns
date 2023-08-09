f97f20b9243b10531a721bcb51fd27b7d529c7ab
Oseib4 <oseibn4@gmail.com>
2023-08-09 18:45:19 +0200

    modified 7-reverse-engieering

--- 7-reverse-engineering/level-1/duck-duck-search/src/init/index.js
+++ 7-reverse-engineering/level-1/duck-duck-search/src/init/index.js
@@ -1,2 +1,9 @@
 // this file is the program's Entry Point
 debugger;
+
+const inputEL = document.getElementById('new-color');
+
+input.addEventListener('change', (e) => {
+    const wrapper = document.getElementById('user-interface');
+    wrapper.style.background = e.target.value;
+});
